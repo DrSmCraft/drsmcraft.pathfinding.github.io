d45142f401c69209afa8431201818d350ad97442
Linux <samuil2000@hotmail.com>
2021-09-15 01:32:32 -0400

    Adding controls and themes

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function setThemeLight() {
+    let link = document.getElementById("theme-style");
+    link.setAttribute("href", "css/style-light.css");
+    return false;
+}
+
+function setThemeDark() {
+    let link = document.getElementById("theme-style");
+    link.setAttribute("href", "css/style-dark.css");
+    return false;
+}
