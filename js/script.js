18544f5e04bd1dc03bb057f9bd908177cdfecdb9
Linux <samuil2000@hotmail.com>
2021-09-16 15:54:09 -0400

    Adding more controls

--- js/script.js
+++ js/script.js
@@ -3,44 +3,71 @@ const SPACING = 30;
 var mousePosX = -1;
 var mousePosY = -1;
 var size = 700;
-var obstacles = []
+var obstacles = {}
 
 var startFocused = false;
 var endFocused = false;
 var obstacleFocused = false;
+var mouseDragging = false;
+var ctrlDown = false;
 
-
-function startFocus() {
+function startFocus(e) {
     startFocused = true;
     endFocused = false;
     obstacleFocused = false;
 }
 
-function endFocus() {
+function endFocus(e) {
     startFocused = false;
     endFocused = true;
     obstacleFocused = false;
 }
 
-function obstacleFocus() {
+function obstacleFocus(e) {
     startFocused = false;
     endFocused = false;
     obstacleFocused = true;
 
 }
 
-function noFocus(){
+function noFocus(e) {
     startFocused = false;
     endFocused = false;
     obstacleFocused = false;
 }
-function outputClicked(e) {
 
+function outputOnMouseDown(e) {
+    mouseDragging = true;
+}
+
+
+function outputOnMouseUp(e) {
+    mouseDragging = false;
+}
+
+function outputKeyDown(e) {
+    if (e.key === "Control") {
+        ctrlDown = true;
+    }
+}
+
+function outputKeyUp(e) {
+    if (e.key === "Control") {
+        ctrlDown = false;
+    }
+}
+
+function hashPosition(x, y) {
+    return (x * 0x1f1f1f1f) ^ y;
+}
+
+
+function handleMouse(e, dragging) {
     let sender = e.target || e.srcElement;
     let posX = mousePosXToGridX(e.x, sender.getBoundingClientRect().x);
     let posY = mousePosYToGridY(e.y, sender.getBoundingClientRect().y);
 
-    if (startFocused) {
+    if (startFocused && dragging) {
         let startXInput = document.getElementById("start-x-input");
         let startYInput = document.getElementById("start-y-input");
 
@@ -49,7 +76,7 @@ function outputClicked(e) {
         startYInput.value = posY;
         return;
     }
-    if (endFocused) {
+    if (endFocused && dragging) {
         let endXInput = document.getElementById("end-x-input");
         let endYInput = document.getElementById("end-y-input");
 
@@ -58,14 +85,28 @@ function outputClicked(e) {
         endYInput.value = posY;
         return;
     }
-    if (obstacleFocused) {
-        let point = [posX, posY];
-        if (obstacles.indexOf(point) === -1) {
-            obstacles.push(point);
+    if (obstacleFocused && dragging) {
+        let point = {x: posX, y: posY};
+        let hash = hashPosition(posX, posY);
+        let exists = hash in obstacles;
+
+
+        if (ctrlDown) {
+            if (exists) {
+                delete obstacles[hash];
+            }
+            return;
+        }
+        if (!exists) {
+            obstacles[hash] = point;
         }
 
-        return;
+
     }
+}
+
+function outputClicked(e) {
+    handleMouse(e, true);
 
 }
 
@@ -90,12 +131,25 @@ function mousePosYToGridY(y, offsetY) {
 function setThemeLight() {
     let link = document.getElementById("theme-style");
     link.setAttribute("href", "css/style-light.css");
+
+    let date = new Date();
+    date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+
+    document.cookie = "theme=light; " + expires + "; path=/";
     return false;
 }
 
 function setThemeDark() {
     let link = document.getElementById("theme-style");
     link.setAttribute("href", "css/style-dark.css");
+
+
+    let date = new Date();
+    date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+
+    document.cookie = "theme=dark; " + expires + "; path=/";
     return false;
 }
 
@@ -103,7 +157,7 @@ function setThemeDark() {
 function drawGrid(outputContainer, output, stage) {
 
     size = Math.min(outputContainer.clientWidth, outputContainer.clientHeight);
-    size = clamp(700, 1000, size);
+    size = clamp(700, 800, size);
     size = Math.floor(size / SPACING) * SPACING
     output.setAttribute("width", size);
     output.setAttribute("height", size);
@@ -146,11 +200,8 @@ function drawMouseOverlay(outputContainer, output, stage) {
 }
 
 function mouseMoveOverOutput(e) {
-    let sender = e.target || e.srcElement;
-    mousePosX = mousePosXToGridX(e.x, sender.getBoundingClientRect().x);
-    mousePosY = mousePosYToGridY(e.y, sender.getBoundingClientRect().y);
 
-
+    handleMouse(e, mouseDragging);
 }
 
 
@@ -170,9 +221,10 @@ function drawEndOverlay(outputContainer, output, stage, posX, posY) {
 function drawObstacles(outputContainer, output, stage) {
     var shape = new createjs.Shape();
 
-    for (let i = 0; i < obstacles.length; i++) {
-        let pt = obstacles[i];
-        shape.graphics.beginFill("grey").drawRect(pt[0] * SPACING, pt[1] * SPACING, SPACING, SPACING);
+    for (let key in obstacles) {
+
+        let pt = obstacles[key];
+        shape.graphics.beginFill("grey").drawRect(pt.x * SPACING, pt.y * SPACING, SPACING, SPACING);
 
     }
     stage.addChild(shape);
@@ -180,9 +232,47 @@ function drawObstacles(outputContainer, output, stage) {
 }
 
 
+function disableSettings() {
+    document.getElementById("poly-radio").setAttribute("disabled", "");
+    document.getElementById("dijkstra-radio").setAttribute("disabled", "");
+    document.getElementById("aStar-radio").setAttribute("disabled", "");
+    document.getElementById("grid-radio").setAttribute("disabled", "");
+
+    document.getElementById("poly-radio").setAttribute("disabled", "");
+    document.getElementById("start-x-input").setAttribute("disabled", "");
+    document.getElementById("start-y-input").setAttribute("disabled", "");
+    document.getElementById("end-x-input").setAttribute("disabled", "");
+    document.getElementById("end-y-input").setAttribute("disabled", "");
+    document.getElementById("obstacle-listbox").setAttribute("disabled", "");
+
+}
+
+
+function enableSettings() {
+    document.getElementById("poly-radio").removeAttribute("disabled");
+    document.getElementById("dijkstra-radio").removeAttribute("disabled");
+    document.getElementById("aStar-radio").removeAttribute("disabled");
+    document.getElementById("grid-radio").removeAttribute("disabled");
+
+    document.getElementById("poly-radio").removeAttribute("disabled");
+    document.getElementById("start-x-input").removeAttribute("disabled");
+    document.getElementById("start-y-input").removeAttribute("disabled");
+    document.getElementById("end-x-input").removeAttribute("disabled");
+    document.getElementById("end-y-input").removeAttribute("disabled");
+    document.getElementById("obstacle-listbox").removeAttribute("disabled");
+
+}
+
+
+
+function startVisualization(){
+    disableSettings();
+}
+
 createjs.Ticker.addEventListener("tick", mainLoop);
 
 function mainLoop(e) {
+
     let startXInput = document.getElementById("start-x-input");
     let startYInput = document.getElementById("start-y-input");
     startXInput.setAttribute("max", size / SPACING - 1);
@@ -208,6 +298,17 @@ function mainLoop(e) {
         let option = document.createElement("option");
         option.innerText = "(" + obstacles[i][0] + ", " + obstacles[i][1] + ")";
         obstacleListbox.appendChild(option);
+    }
+
+
+    for (let key in obstacles) {
+        let pt = obstacles[key];
+        let option = document.createElement("option");
+        option.innerText = "(" + pt.x + ", " + pt.y + ")";
+
+        obstacleListbox.appendChild(option);
+
+
     }
 
     var outputContainer = document.getElementById("output-container");
